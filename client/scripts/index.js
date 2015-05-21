171cf5cfed9e098a15e704b1bb03c1c7c442f0ab
Viengo <viengooleg@gmail.com>
2015-05-21 23:00:05 +0400

    faq section, tabs

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,3 +1,9 @@
 $(window).on("scroll touchmove", function () {
     $('#header-nav').toggleClass('scroll-open', $(document).scrollTop() > 0);
 });
+
+$('#myTab a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+    $('#myTab a[href="#meals"]').tab('show')
+})
